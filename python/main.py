eae4880aeaad2f08c78dd8d3a8bb87f35b910bc5
Zbysekz <zbysekzapadlik@seznam.cz>
2019-08-20 04:39:07 -0400

    Put auxiliary functions in separated file

--- python/main.py
+++ python/main.py
@@ -3,13 +3,14 @@
 
 import os.path
 import yaml
-import math
 import htm2d.environment
 import htm2d.agent
 from htm2d.agent import Direction
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+
+from utils import plotBinaryMap,isNotebook # auxiliary functions from utils.py
   
 from htm.bindings.algorithms import SpatialPooler, TemporalMemory
 from htm.bindings.sdr import SDR, Metrics
@@ -159,35 +160,7 @@ def SystemCalculate():
   plt.show(block=False)
   plt.pause(0.01)# delay is needed for proper redraw
   
-def plotBinaryMap(axes, name, data):
-  
-  axes.set_title(name)
-  axes.set_xlabel('Rows')
-  axes.set_ylabel('Columns')
-  
-  plotW = math.ceil(math.sqrt(len(data)))
 
-  rf = np.zeros([ plotW, plotW ], dtype=np.uint8)
-  for i in range(plotW):
-    arr = data[i*plotW:i*plotW+plotW]*2
-    if len(arr)<plotW:
-      arr=np.concatenate([arr, np.ones(plotW-len(arr))])
-    rf[:, i] = arr
-    
-  axes.imshow(rf, interpolation='nearest')
-  
-def isNotebook():
-  try:
-      shell = get_ipython().__class__.__name__
-      if shell == 'ZMQInteractiveShell':
-          return True   # Jupyter notebook or qtconsole
-      elif shell == 'TerminalInteractiveShell':
-          return False  # Terminal running IPython
-      else:
-          return False  # Other type (?)
-  except NameError:
-      return False      # Probably standard Python interpreter
-  
 if __name__ == "__main__":
    
   # load model parameters from file
